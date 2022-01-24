a29f247a1bea77a04994dd5f8ae28377f1d77f8b
Deokyong Kim <yh100112@naver.com>
2022-01-24 16:35:12 +0900

    java와 oracle jdbc로 연동 후 CRUD 구현

--- src/OracleConnection/OracleConn.java
+++ src/OracleConnection/OracleConn.java
@@ -7,6 +7,26 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class OracleConn {
+	
+	public static void select_table(Connection con,PreparedStatement pstmt,ResultSet rs) {
+		try {
+			String sql = "select * from test";
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				String name = rs.getString("name");
+				int age = rs.getInt("age");
+				
+				System.out.println(name + "\t" + age);
+			}	
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		String driver = "oracle.jdbc.driver.OracleDriver";
 		String url = "jdbc:oracle:thin:@localhost:1521:oracle";
@@ -24,32 +44,29 @@ public class OracleConn {
 			
 			//select
 			String sql = "select * from test";
-			pstmt = con.prepareStatement(sql);
-			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				String name = rs.getString("name");
-				int age = rs.getInt("age");
-				
-				System.out.println(name + "\t" + age);
-			}
+			select_table(con,pstmt,rs);
 			System.out.println("-----------------------------");
-			
 			
 			//insert
 			sql = "insert into test values(?,?)";
 			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, "KIM BAE BAE");
+			pstmt.setString(1, "hong gil dong");
 			pstmt.setInt(2, 33);
 			int n = pstmt.executeUpdate();
-			System.out.println(n + "개의 row 저장");
+			
+			System.out.println("데이터 삽입 후 테이블 조회");
+			select_table(con,pstmt,rs);
+			
 			System.out.println("-----------------------------");
 			
 			//update
-			sql = "update test set age=999 where name='KIM DEOK YONG'";
+			sql = "update test set age=999 where name='hong gil dong'";
 			pstmt = con.prepareStatement(sql);
 			int num = pstmt.executeUpdate();
-			System.out.println("업데이트된 row 갯수 : " + num);
+			
+			System.out.println("업데이트 후 테이블 조회");
+			select_table(con, pstmt, rs);
+			
 			
 		}catch(ClassNotFoundException e) {
 			System.out.println("jdbc driver 로딩 실패");
@@ -57,9 +74,9 @@ public class OracleConn {
 			System.out.println("오라클 연동 실패");
 		}finally {
 			try {
-				rs.close();
-				pstmt.close();
-				con.close();
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(con != null) con.close();
 			}catch(SQLException e) {
 				e.getStackTrace();
 			}
