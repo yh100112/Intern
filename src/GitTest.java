841612d2f09843213e6fa2407e0c0684ed44d32f
Deokyong Kim <yh100112@naver.com>
2022-01-24 13:37:04 +0900

    third commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,6 +1,8 @@
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Git test");
+		System.out.println("Git test");
+		System.out.println("Git test");
 		//first commit
 	}
 }
