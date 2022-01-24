3490608025406fdd52578e9111b22b7a0ccacc87
Deokyong Kim <yh100112@naver.com>
2022-01-24 16:58:36 +0900

    oracle 연동 코드 주석 정리

--- src/OracleConnection/OracleConn.java
+++ src/OracleConnection/OracleConn.java
@@ -12,10 +12,10 @@ public class OracleConn {
 		try {
 			String sql = "select * from test";
 			pstmt = con.prepareStatement(sql);
-			rs = pstmt.executeQuery();
+			rs = pstmt.executeQuery(); // sql명령어 결과를 ResultSet 객체에 저장   select -> executeQuery() , update,delete,insert -> executeUpdate()
 			
-			while(rs.next()) {
-				String name = rs.getString("name");
+			while(rs.next()) { // 테이블 값들을 한 row씩 접근
+				String name = rs.getString("name"); // column이 name인 곳의 데이터를 타입에 맞게 가져옴
 				int age = rs.getInt("age");
 				
 				System.out.println(name + "\t" + age);
@@ -38,8 +38,8 @@ public class OracleConn {
 		ResultSet rs = null;
 		
 		try {
-			Class.forName(driver);
-			con = DriverManager.getConnection(url,userid,passwd);
+			Class.forName(driver); // ojdbc8.jar에 있는 드라이버를 메모리에 로딩
+			con = DriverManager.getConnection(url,userid,passwd); // 오라클 연동 (디비가 있는 url, 접속할 유저 아디 ,접속할 유저 비번)
 			
 			
 			//select
@@ -49,9 +49,9 @@ public class OracleConn {
 			
 			//insert
 			sql = "insert into test values(?,?)";
-			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1, "hong gil dong");
-			pstmt.setInt(2, 33);
+			pstmt = con.prepareStatement(sql);		// sql명령어를 실행해줄  PreparedStatement 객체
+			pstmt.setString(1, "lee sun sin");
+			pstmt.setInt(2, 26);
 			int n = pstmt.executeUpdate();
 			
 			System.out.println("데이터 삽입 후 테이블 조회");
@@ -60,7 +60,7 @@ public class OracleConn {
 			System.out.println("-----------------------------");
 			
 			//update
-			sql = "update test set age=999 where name='hong gil dong'";
+			sql = "update test set age=44 where name='hong gil dong'";
 			pstmt = con.prepareStatement(sql);
 			int num = pstmt.executeUpdate();
 			
